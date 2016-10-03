4ae51709714ec997ef0a657c2e67e99ebd08cfd5
Jozef Izso <jozef.izso@gmail.com>
2016-10-03 15:44:24 +0200

    Remove aurelia-testing plugin

--- src/AureliaWebApp/src/main.ts
+++ src/AureliaWebApp/src/main.ts
@@ -18,9 +18,5 @@ export function configure(aurelia: Aurelia) {
     aurelia.use.developmentLogging();
   }
 
-  if (environment.testing) {
-    aurelia.use.plugin('aurelia-testing');
-  }
-
   aurelia.start().then(() => aurelia.setRoot());
 }
